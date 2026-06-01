4b28806c96b9d35c48e912aa1013b1508321ac9e
Matvey0808 <avto.mat.2000g@gmail.com>
2026-06-01 21:25:54 +0700

    fix save state when switch views

--- lib/Navigation/main_navigation.dart
+++ lib/Navigation/main_navigation.dart
@@ -17,32 +17,25 @@ class _MainNavigationViewState extends State<MainNavigationView> {
   Widget build(BuildContext context) {
     final iconNavBar = [Icons.settings, Icons.chrome_reader_mode, Icons.person];
 
-    final view = [
-      SettingsView(key: ValueKey(cur)),
-      HomeView(key: ValueKey(cur)),
-      ProfileView(key: ValueKey(cur)),
+    final views = [
+      FadeSlideTransition(
+        visible: cur == 0,
+        child: const SettingsView(key: PageStorageKey('settings')),
+      ),
+      FadeSlideTransition(
+        visible: cur == 1,
+        child: const HomeView(key: PageStorageKey('home')),
+      ),
+      FadeSlideTransition(
+        visible: cur == 2,
+        child: const ProfileView(key: PageStorageKey('profile')),
+      ),
     ];
-
-    final animNavView = view.map((anim) {
-      return AnimatedSwitcher(
-        switchInCurve: Curves.easeOut,
-        switchOutCurve: Curves.easeIn,
-        duration: const Duration(milliseconds: 500),
-        transitionBuilder: (child, animation) => SlideTransition(
-          position: Tween<Offset>(
-            begin: const Offset(0, -4),
-            end: Offset.zero,
-          ).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut)),
-          child: FadeTransition(opacity: animation, child: child),
-        ),
-        child: view[cur],
-      );
-    }).toList();
 
     return Scaffold(
       body: Stack(
         children: [
-          IndexedStack(index: cur, children: animNavView),
+          IndexedStack(index: cur, children: views),
           Positioned(
             left: 98,
             right: 98,
@@ -84,6 +77,75 @@ class _MainNavigationViewState extends State<MainNavigationView> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// FIXME: Временный класс анимации при переключении экранов. Нужно заменить на свой
+class FadeSlideTransition extends StatefulWidget {
+  const FadeSlideTransition({
+    super.key,
+    required this.child,
+    required this.visible,
+  });
+
+  final Widget child;
+  final bool visible;
+
+  @override
+  State<FadeSlideTransition> createState() => _FadeSlideTransitionState();
+}
+
+class _FadeSlideTransitionState extends State<FadeSlideTransition>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<Offset> _offsetAnimation;
+  late final Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _fadeAnimation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    if (widget.visible) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant FadeSlideTransition oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.visible) {
+      _controller.forward(from: 0.5);
+    } else {
+      _controller.reset();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: FadeTransition(
+        opacity: _fadeAnimation,
+        child: widget.child,
       ),
     );
   }
